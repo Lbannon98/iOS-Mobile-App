50f356177a99e8fed26ae054924ac394c1873931
Lauren Bannon <lbannon@zendesk.com>
2019-03-23 14:56:38 +0000

    Added the UIView assets

---
+++ hustle-mode/hustle-mode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Lauren Bannon on 23/03/2019.
+//  Copyright © 2019 Lauren Bannon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
